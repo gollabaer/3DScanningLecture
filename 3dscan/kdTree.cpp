a9604db970c65d34bb0ebf9f4b68781433aae503
alena-kathrin.schnurr <alena-kathrin.schnurr@st.ovgu.de>
2015-11-01 12:38:36 +0100

    added range query method, not working yet

--- 3dscan/kdTree.cpp
+++ 3dscan/kdTree.cpp
@@ -11,7 +11,6 @@ kdTree::kdTree(std::vector<float> points, int maxDepth, int dim)
 	m_MaxDepth = maxDepth;
 	m_Points = points;
 	m_Dim = dim;
-	m_Points = points;
 	std::vector<int> indices;
 	
 	indices.resize(points.size() / 3);
@@ -20,12 +19,14 @@ kdTree::kdTree(std::vector<float> points, int maxDepth, int dim)
 	{
 		*it = help++;
 	}
-	m_Root = &Node(indices, points, maxDepth, dim, NULL);
+	m_Root = &Node(indices, m_Points, m_MaxDepth, m_Dim, NULL);
 }
 
 kdTree::~kdTree()
 {
 };
+
+// Node Class
 
 kdTree::Node::Node()
 {
@@ -33,35 +34,81 @@ kdTree::Node::Node()
 
 kdTree::Node::Node(std::vector<int> indices, std::vector<float> &points, int depth, int dim, Node* parent)
 {
-	int axis = depth % dim;
+	// choose current axis
+	int axis = depth % dim; // 0 = x, 1 = y, 2 = z
+
+	// assign parent and intialize children
 	m_Parent = parent;	
+	m_LeftChild = NULL;
+	m_RightChild = NULL;
 
 	// sort by axis
 	std::sort(begin(indices), end(indices), [&](size_t a, size_t b) { return points[a+axis] < points[b+axis]; });
+	m_Indices = indices;
 
 	// choose median
-	m_Median = points[indices[indices.size() / 2]-1];
+	m_Median = points[indices[indices.size() / 2]-1 + axis];
+
+	// set Min and Max
+	m_Max = points[indices[0] + axis];
+	m_Min = points[indices.size()-1 + axis];
 
 	// split indices
 	std::vector<int> indicesLeft, indicesRight;
 	indicesLeft.clear();
 	indicesRight.clear();
 
-	for (int iter = 0; iter < indices.size(); iter++)
+	for (std::vector<int>::iterator it = indices.begin(); it != indices.end(); ++it)
 	{
-		if (iter <= indices.size() / 2-1)	indicesLeft.push_back(indices[iter]);
-		else indicesRight.push_back(indices[iter]);
+		if (points[*it+axis] <= m_Median)	indicesLeft.push_back(*it);
+		else indicesRight.push_back(*it);
 	}
+
 	// construct left and right child
 	if(depth > 1)
 	{
-		m_LeftChild = &Node(indicesLeft, points, depth - 1, dim, this);
-		m_RightChild = &Node(indicesRight, points, depth - 1, dim, this);
+		if (indicesLeft.size() > 1)	m_LeftChild = &Node(indicesLeft, points, depth - 1, dim, this);
+		if (indicesRight.size() > 1) m_RightChild = &Node(indicesRight, points, depth - 1, dim, this);
 	}
 
 }
 kdTree::Node::~Node()
 {
+}
+
+kdTree::Node* kdTree::Node::getParent()
+{
+	return m_Parent;
+}
+
+kdTree::Node* kdTree::Node::getLeftChild()
+{
+	return m_LeftChild;
+}
+
+kdTree::Node* kdTree::Node::getRightChild()
+{
+	return m_RightChild;
+}
+
+int kdTree::Node::getMedian()
+{
+	return m_Median;
+}
+
+float kdTree::Node::getMax()
+{
+	return m_Max;
+}
+
+float kdTree::Node::getMin()
+{
+	return m_Min;
+}
+
+std::vector<int> kdTree::Node::getIndices()
+{
+	return m_Indices;
 }
 
 
@@ -75,4 +122,70 @@ int kdTree::getNumberOfPoints()
 std::vector<float> kdTree::getPoints()
 {
 	return m_Points;
+}
+
+// Functions
+
+std::vector<float> kdTree::rangeQuery(QVector3D p1, QVector3D p2)
+{
+	return traverse(m_Root, m_MaxDepth, m_Points, p1, p2);
+}
+
+std::vector<float> kdTree::traverse(Node* node, int depth, std::vector<float> &points, QVector3D p1, QVector3D p2)
+{
+	// choose current axis
+	int axis = depth % m_Dim; // 0 = x, 1 = y, 2 = z
+
+	// if both points are equal/smaller than median, traverse left child
+	if (p1[axis] <= node->getMedian() && p2[axis] <= node->getMedian() && node->getLeftChild != NULL)
+	{
+		return traverse(node->getLeftChild(), depth - 1, points, p1, p2);
+	}
+
+	// if both points are bigger than median, traverse right child
+	if (p1[axis] > node->getMedian() && p2[axis] > node->getMedian() && node->getRightChild != NULL)
+	{
+		return traverse(node->getRightChild(), depth - 1, points, p1, p2);
+	}
+
+	std::vector<float> result;
+	result.clear;
+
+	if (node->getLeftChild != NULL)
+	{
+		result = traverse(node->getLeftChild(), depth - 1, points, p1, p2);
+	}
+
+	if (node->getRightChild() != NULL)
+	{
+		std::vector<float> resultRight = traverse(node->getRightChild(), depth - 1, points, p1, p2);
+		// Combine results of left and right child
+		result.reserve(result.size() + resultRight.size()); // preallocate memory
+		result.insert(result.end(), resultRight.begin(), resultRight.end());
+	}
+
+	if (node->getLeftChild == NULL)
+	{
+		std::vector<int> indices = node->getIndices();
+		for (std::vector<int>::iterator it = indices.begin(); it != indices.end(); ++it)
+		{
+			if (points[*it + axis] <= node->getMedian())
+			{
+				if (points[*it + axis])
+				result.push_back(*it);
+			}
+			else break;
+		}
+	}
+
+	if (node->getRightChild == NULL)
+	{
+		std::vector<int> indices = node->getIndices();
+		for (std::vector<int>::iterator it = indices.end(); it != indices.begin(); --it)
+		{
+			if (points[*it + axis] > node->getMedian()) result.push_back(*it);
+			else break;
+		}
+	}
+
 }
--- 3dscan/kdTree.h
+++ 3dscan/kdTree.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>  
-#include <QDebug>
+#include <QVector3D>
+
 
 #pragma once
 class kdTree
@@ -11,8 +12,10 @@ public:
 	kdTree(std::vector<float> points, int maxDepth, int dim);
 	~kdTree();
 	// Getter
-	int getNumberOfPoints();
-	std::vector<float> getPoints();
+	int getNumberOfPoints(); // returns the number of points stored in the kd Tree
+	std::vector<float> getPoints(); // returns a vector containing all stored points
+	// Functions
+	std::vector<float> rangeQuery(QVector3D p1, QVector3D p2); // returns all points inside the box formed by p1 and p2
 private:
 	// Node Class
 	class Node
@@ -22,6 +25,14 @@ private:
 		Node();
 		~Node();
 		Node(std::vector<int> indices, std::vector<float> &points, int depth, int dim, Node* parent);
+		// Getter
+		Node* getParent();
+		Node* getLeftChild();
+		Node* getRightChild();
+		int getMedian();
+		float getMax();
+		float getMin();
+		std::vector<int> getIndices();
 	private:
 		// Member Variables
 		Node* m_Parent; // Parent Node
@@ -37,5 +48,7 @@ private:
 	std::vector<float> m_Points;
 	int m_MaxDepth;
 	int m_Dim;
+	// Functions
+	std::vector<float> traverse(Node* node, int depth, std::vector<float> &points, QVector3D p1, QVector3D p2);
 };
 
